0527dbb18134b96039ce2feae93546fc902926f7
helenwxdong <helenwxdong@gmail.com>
2019-09-14 02:20:05 -0400

    Helen made Header and Description on font-end

---
+++ my-app/src/components/Description.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Description() {
+    return (
+        <header style={desStyle}>
+            <p>Here's a brief discription of what our shit does.
+                Please use it fiodsjfisjdifjajdsfsdf ahahasdfkl. thx</p>
+        </header>
+
+    )
+}
+
+const desStyle = {
+    padding:'0.5vw 4vw',
+    textAlign: 'center',
+    fontFamily: 'Quicksand',
+    fontSize: '25px',
+    backgroundColor: '#d6d6f5'
+}
+export default Description;
